2540931d775eb4570553eba45df7599fb8b183f3
Mahendra Choudhary <mahendrachoudhary1156@gmail.com>
2023-08-18 21:36:42 +0530

    added method to fetch classmates of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
   has_many :enrollments
   has_many :teachers, through: :enrollments
   has_many :programs, through: :enrollments
+
+  class << self
+    def classmates(user)
+      classmate_ids = Enrollment
+                    .where(program_id: user.program_ids)
+                    .where
+                    .not(user: user )
+                    .map(&:user_id)
+
+      User.where(id: classmate_ids)
+    end
+  end
 end
